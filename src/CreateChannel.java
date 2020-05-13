10353e86744472b425c6dbbb895e261403c372c5
halilibrahimculcu <halilibrahimculcu@yahoo.com>
2020-05-13 16:00:13 +0100

    update CreateChannel

--- src/CreateChannel.java
+++ src/CreateChannel.java
@@ -1,10 +1,14 @@
 public class CreateChannel {
     public static void main(String[] args) {
 
-        int a = 3;
+            int a = 9;
+            int b = 8;
+                if (a < b ){
+            System.out.println("bakali ne olaca");
+        }
+
+
+        int x = 3;
         System.out.println("TO BE OR NOT TO BE");
-
-
-
     }
 }
